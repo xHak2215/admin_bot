6f53e4d556d3b468800660a8880ca6b34187b385
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2026-01-27 19:33:07 +0300

    Update ai_api_lib.py
    улутшение бибиотеки возаимоействия с ai api

--- asets/ai_api_lib.py
+++ asets/ai_api_lib.py
@@ -1,23 +1,36 @@
 import requests
 
-timeout=5
 
-def chec_spam(message:str)->dict|None:
-    response = requests.get(r"http://127.0.0.2:8080/spam", 
-                            params={'message': message},timeout=timeout)
-    try:
-        response.raise_for_status()
-        relust_spam = response.json()
-        return relust_spam
-    except Exception:
-        return None
+class api_config:
+    def __init__(self) -> None:
+        self.timeout=5
+        self.root_url="http://127.0.0.2:8080"
 
-def chec_insult(message:str)->dict|None:
-    response = requests.get(r"http://127.0.0.2:8080/affront_detect", 
-                            params={'message': message},timeout=timeout)
-    try:
-        response.raise_for_status()
-        relust_affront = response.json()
-        return relust_affront
-    except Exception:
-        return None
+    def get_api_status(self)->str|None:
+        response = requests.get(self.root_url, timeout=self.timeout)
+        response.close()
+        return response.content.decode()
+
+    def chec_spam(self, message:str)->dict|None:
+        response = requests.get(f"{self.root_url}/spam", 
+                                params={'message': message}, timeout=self.timeout)
+        try:
+            response.raise_for_status()
+            relust_spam = response.json()
+            response.close()
+            return relust_spam
+        except Exception:
+            return None
+
+    def chec_insult(self, message:str)->dict|None:
+        response = requests.get(f"{self.root_url}/affront_detect", 
+                                params={'message': message}, timeout=self.timeout)
+        try:
+            response.raise_for_status()
+            relust_affront = response.json()
+            response.close()
+            return relust_affront
+        except Exception:
+            return None
+
+ai_api=api_config()
